507b7d1649ee6638a01f0a8ca1fabd81be7e992d
xpilgrim <xpilger@xmobile.(none)>
2013-11-06 15:04:25 +0100

    added update_nautilus_scripts_ubuntu_13_local.sh

---
+++ update_nautilus_scripts_ubuntu_13_local.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Dieses kleine Script uebernimmt das Update von nautilus-scripts
+# Aufruf in dem Verzeichnis, in dem dieses
+# und die zu aktualiseirenden Scripts liegen.
+#
+# Benutzung ab ubuntu 13.04!
+#
+# This script update nautilus-scripts in the current directory.
+# since ubuntu 13.04
+#
+# Author: Joerg Sorge
+# Distributed under the terms of GNU GPL version 2 or later
+# Copyright (C) Joerg Sorge joergsorge at googell
+# 2012-06-13
+
+
+echo "Nautilus-Scripts aktualisieren..."
+
+for i in *.sh
+do
+   :
+	if [ "$i" != "update_nautilus_scripts.sh" ] 
+		then	
+		echo "$i"
+		cp $i /home/$USER/.local/share/nautilus/scripts/
+		chmod +x /home/$USER/.local/share/nautilus/scripts/$i
+	fi
+done
+
+echo "finito"
+exit
