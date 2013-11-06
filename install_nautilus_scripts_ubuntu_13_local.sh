8031f09411c7945a9641595535befd54cbc78f75
xpilgrim <xpilger@xmobile.(none)>
2013-11-06 15:19:50 +0100

    added install_nautilus_scripts_ubuntu_13_local.sh

---
+++ install_nautilus_scripts_ubuntu_13_local.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Dieses kleine Script uebernimmt die Installation von nautilus-scripts
+# Aufruf in dem Verzeichnis, in dem das zu installierende Script liegt
+#
+# This script install nautilus-scripts from the current directory.
+# since ubuntu 13.04
+#
+# Author: Joerg Sorge
+# Distributed under the terms of GNU GPL version 2 or later
+# Copyright (C) Joerg Sorge joergsorge at googell
+# 2013-07-25
+
+
+echo "Nautilus-Scripts installieren..."
+
+for i in *.sh
+do
+   :
+	if [ "$i" != "install_nautilus_scripts_ubuntu_13_local.sh" ] 
+		then	
+		echo "$i"
+		cp $i /home/$USER/.local/share/nautilus/scripts/
+		chmod +x /home/$USER/.local/share/nautilus/scripts/$i
+	fi
+done
+echo "Libs installieren..."
+sudo apt-get install \
+lame mp3val libid3-tools mp3gain mp3info sox ffmpeg libsox-fmt-mp3 \
+curl gawk links libtranslate-bin
+
+# nautius reset
+nautilus -q
+
+echo "finito"
+exit
