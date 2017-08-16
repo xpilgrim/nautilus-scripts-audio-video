e90b334245d651c42744855e22c8de9c55aebccd
Joerg Sorge <joergsorge@gmail.com>
2017-08-16 15:15:17 +0200

    added en de msg switch

--- wav_to_mp3_m_128.sh
+++ wav_to_mp3_m_128.sh
@@ -24,32 +24,62 @@
 # Copyright (C) Joerg Sorge joergsorge@gmail.com
 # 2011-09-01
 
+function f_choose_msg_lang () {
+	local_locale=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
+	if [ $local_locale == "de" ]; then
+		msg[1]="installiert"
+		msg[2]="Enkodieren zu mp3..."
+		msg[3]="replayGain berechnen..."
+		msg[4]="wav zu mp3: Bearbeite Dateien..."
+		msg[5]="Abgebrochen..."
+
+		err[1]=" ist nicht installiert, Bearbeitung nicht moeglich."
+		err[2]="Das ist keine mp3 Datei:"
+		err[3]="Fehler beim enkodieren"
+		err[4]="Fehler bei mp3gain"
+	else
+		msg[1]="installed"
+		msg[2]="Encode to mp3..."
+		msg[3]="Calculate replayGain..."
+		msg[4]="wav to mp3: work on files..."
+		msg[5]="Canceled..."
+
+		err[1]=" not installed, work not possible."
+		err[2]="It's not a wav file:"
+		err[3]="Error by encoding to mp3 "
+		err[4]="Error by mp3gain"
+	fi
+}
+
+
 function f_check_package () {
 	package_install=$1
 	if dpkg-query -s $1 2>/dev/null|grep -q installed; then
 		echo "$package_install installiert"
 	else
-		zenity --error --text="Paket $package_install ist nicht installiert, Bearbeitung nicht moeglich." 
+		zenity --error --text="$package_install ${err[1]}" 
 		exit
 	fi
 }
+
+
+# switch lang
+f_choose_msg_lang
+# check for packages
+f_check_package "lame"
 
 echo -n "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}" | while read file ; do
 
 report "wavtomp3"
 (
-
-	# check for packages
-	f_check_package "lame"
-
 	filename=$(basename "$file")
 	extension="${filename##*.}"
 	# echo and progress will pulsate
 	echo "10"
-	echo "# Encode to mp3...\n$filename"
+	echo "# ${msg[2]}\n$filename"
 	
 	if [ "$extension" != "wav" ] && [ "$extension" != "WAV" ]; then
-		zenity --error --text="It's not a wav file:\n$filename" 
+		zenity --error --text="${err[2]}\n$filename" 
 		exit
 	fi
 
@@ -59,14 +89,14 @@ report "wavtomp3"
 	error=${message##*S}
 	if [ "$error" != "uccess" ]
 		then
-		echo "$message" | zenity --title="Error by encoding to mp3 " --text-info --width=500 --height=200
+		echo "$message" | zenity --title="${err[3]}" --text-info --width=500 --height=200
 	fi
 
 ) | zenity --progress \
-           --title="wav to mp3: Work on files" --text="..." --width=500 --pulsate --auto-close
+           --title="${msg[4]}" --text="..." --width=500 --pulsate --auto-close
 
 if [ "$?" = -1 ] ; then
-	zenity --error --text="Bearbeitung abgebrochen"
+	zenity --error --text="${msg[5]}"
 fi
 done
 exit
