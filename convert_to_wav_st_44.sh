dd2115f01d12f59f08f518baaca7ff44e4caa758
Joerg Sorge <joergsorge@gmail.com>
2017-08-21 15:18:30 +0200

    added lang switch

--- convert_to_wav_st_44.sh
+++ convert_to_wav_st_44.sh
@@ -19,17 +19,47 @@
 # Copyright (C) Joerg Sorge joergsorge@gmail.com
 # 2012-08-01
 
-function f_check_package () {
-	package_install=$1
-	if dpkg-query -s $1 2>/dev/null|grep -q installed; then
-		echo "$package_install installiert"
+
+function f_choose_msg_lang () {
+	local_locale=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
+	if [ $local_locale == "de" ]; then
+		msg[1]="installiert"
+		msg[2]="Transkodieren zu wav..."
+		msg[3]="replayGain berechnen..."
+		msg[4]="x zu wav: Bearbeite Dateien..."
+		msg[5]="Abgebrochen..."
+
+		err[1]=" ist nicht installiert, Bearbeitung nicht moeglich."
+		err[2]="Das ist keine wav Datei:"
+		err[3]="Fehler beim Transkodieren"
+		err[4]="Fehler beim Transkodieren"
 	else
-		zenity --error --text="Paket $package_install ist nicht installiert, Bearbeitung nicht moeglich." 
-		exit
+		msg[1]="installed"
+		msg[2]="Transcode to mp3..."
+		msg[3]="Calculate replayGain..."
+		msg[4]="x to wav: work on files..."
+		msg[5]="Canceled..."
+
+		err[1]=" not installed, work not possible."
+		err[2]="It's not a wav file:"
+		err[3]="Error by transcoding to wav "
+		err[4]="Error by mp3gain"
 	fi
 }
 
 
+function f_check_package () {
+	package_install=$1
+	if dpkg-query -s $1 2>/dev/null|grep -q installed; then
+		echo "$package_install ${msg[1]}"
+	else
+		zenity --error --text="$package_install ${err[1]}"
+		exit
+	fi
+}
+
+# switch lang
+f_choose_msg_lang
 # check for packages
 f_check_package "libav-tools"
 
@@ -43,20 +73,20 @@ report "towav"
 	filename=$(basename "$file")
 	# echo and progress will pulsate
 	echo "10"
-	echo "# Konvertierung in wav...\n$filename"
-	message=$(avconv -y -i "$file" -acodec pcm_s16le -ac 2 -ar 44100 "${file%%.*}.wav" 2>&1 && echo "Ohne_Fehler_beendet")
-	# remove all characters right from 'O'
-	error=${message##*O}
-	if [ "$error" != "hne_Fehler_beendet" ]
+	echo "# ${msg[2]}\n$filename"
+	message=$(avconv -y -i "$file" -acodec pcm_s16le -ac 2 -ar 44100 "${file%%.*}.wav" 2>&1 && echo "Success")
+	# remove all characters right from 'S'
+	error=${message##*S}
+	if [ "$error" != "uccess" ]
 		then
-		echo "$message" | zenity --title="wav-Konvertierungs-Fehler " --text-info --width=500 --height=200
+		echo "$message" | zenity --title="${err[3]} " --text-info --width=500 --height=200
 	fi
 
 ) | zenity --progress \
-           --title="X to wav: Datei bearbeiten" --text="..." --width=500 --pulsate --auto-close
+           --title="${msg[4]}" --text="..." --width=500 --pulsate --auto-close
 
 if [ "$?" = -1 ] ; then
-	zenity --error --text="Bearbeitung abgebrochen"
+	zenity --error --text="${msg[5]}"
 fi
 done
 exit
